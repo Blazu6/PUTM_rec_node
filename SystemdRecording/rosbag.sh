57503d39b9646d41f636094f22b4b0c6fc03e60e
Blazu6 <114349938+Blazu6@users.noreply.github.com>
2025-05-27 18:40:13 +0200

    Update rosbag.sh

--- SystemdRecording/rosbag.sh
+++ SystemdRecording/rosbag.sh
@@ -4,4 +4,4 @@ BAG_DIR="/home/putm/rosbagservice"
 
 mkdir -p "$BAG_DIR"
 
-exec ros2 bag record -a --max-bag-size 1073741824 -o "$BAG_DIR/rosbag_$(date +%Y-%m-%d_%H-%M-%S)"
+exec ros2 bag record -a --max-bag-duration 1800 -o "$BAG_DIR/rosbag_$(date +%Y-%m-%d_%H-%M-%S)"
